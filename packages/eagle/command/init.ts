93d111021981ca801a689e4ad009a44059948e12
toyamarinyon <toyamarinyon@gmail.com>
2022-07-12 22:31:27 +0900

    add typescript into dev dependency

--- packages/eagle/command/init.ts
+++ packages/eagle/command/init.ts
@@ -43,7 +43,7 @@ return 'Hello World';
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
   // install dependencies
-  const devDependencies = ["@cloudflare/workers-types", "wrangler", "@toyamarinyon/eagle"];
+  const devDependencies = ["@cloudflare/workers-types", "wrangler", "@toyamarinyon/eagle", "typescript"];
   sync("pnpm", ["install", "--save-dev", ...devDependencies], {
     stdio: "inherit",
   });
