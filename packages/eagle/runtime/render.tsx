7b4a54cf2987d5c7d3229ee776cb44ae9649d1f1
toyamarinyon <toyamarinyon@gmail.com>
2022-07-14 08:16:49 +0900

    tweak

--- packages/eagle/runtime/render.tsx
+++ packages/eagle/runtime/render.tsx
@@ -51,35 +51,4 @@ export async function render(page: Page) {
   const pageElement = await page.default();
   const string = renderToString(<Document>{pageElement}</Document>);
   return string;
-  // let controller = new AbortController();
-  // let didError = false;
-  // try {
-  //   let stream = await renderToReadableStream(await page.default(), {
-  //     signal: controller.signal,
-  //     onError(error) {
-  //       didError = true;
-  //       console.error(error);
-  //     },
-  //   });
-
-  //   // This is to wait for all Suspense boundaries to be ready. You can uncomment
-  //   // this line if you want to buffer the entire HTML instead of streaming it.
-  //   // You can use this for crawlers or static generation:
-
-  //   // await stream.allReady;
-
-  //   return new Response('hello', {
-  //     status: didError ? 500 : 200,
-  //     headers: { "Content-Type": "text/html" },
-  //   });
-  // } catch (error) {
-  //   console.log(error)
-  //   return new Response(
-  //     '<!doctype html><p>Loading...</p><script src="clientrender.js"></script>',
-  //     {
-  //       status: 500,
-  //       headers: { "Content-Type": "text/html" },
-  //     }
-  //   );
-  // }
 }
