1b777baa34b13962444e7fba42bf64bb74e072e8
toyamarinyon <toyamarinyon@gmail.com>
2022-07-17 10:59:22 +0900

    fix test

--- packages/eagle/runtime/render.test.tsx
+++ packages/eagle/runtime/render.test.tsx
@@ -6,5 +6,12 @@ test("render react component", async () => {
   const page: Page = {
     default: () => <div>Hello</div>,
   };
-  expect(render(page)).toMatchInlineSnapshot('"<html lang=\\"en\\"><head><meta charSet=\\"UTF-8\\"/><meta http-equiv=\\"X-UA-Compatible\\" content=\\"IE=edge\\"/><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1.0\\"/><title>Document</title></head><body><div>Hello</div></body></html>"');
+  expect(render(page, "function hydrate() {}")).toMatchInlineSnapshot(
+    `
+    "<html lang=\\"en\\"><head><meta charSet=\\"UTF-8\\"/><meta http-equiv=\\"X-UA-Compatible\\" content=\\"IE=edge\\"/><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1.0\\"/><title>Document</title></head><body><div id=\\"eagle-root\\"><div>Hello</div></div>
+    <script type=\\"module\\">
+    function hydrate() {}
+    </script></body></html>"
+  `
+  );
 });
