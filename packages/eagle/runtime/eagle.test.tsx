aaafef8b92a217721eca32fe840e828f20d10f8a
toyamarinyon <toyamarinyon@gmail.com>
2022-07-29 18:29:35 +0900

    without session

--- packages/eagle/runtime/eagle.test.tsx
+++ packages/eagle/runtime/eagle.test.tsx
@@ -11,7 +11,9 @@ const routes: Routes<typeof sessionScheme> = {
     }),
     handler: {
       POST: async (args) => {
-        return new Response(`<div>current user id is ${args.session.userId}</div>`);
+        return new Response(
+          `<div>current user id is ${args.session.userId}</div>`
+        );
       },
     },
   }),
@@ -59,5 +61,45 @@ test("createEagle", async () => {
       },
     })
   );
-  expect(await response2.text()).toMatchInlineSnapshot('"<div>current user id is 1</div>"');
+  expect(await response2.text()).toMatchInlineSnapshot(
+    '"<div>current user id is 1</div>"'
+  );
+});
+
+test("without session", async () => {
+  const routes2: Routes = {
+    ["index"]: async () => ({
+      default: (props) => <div>{props.message}</div>,
+      PageProps: () => ({
+        message: "hello world",
+      }),
+      handler: {
+        POST: async () => {
+          return new Response(`<div>without session</div>`);
+        },
+      },
+    }),
+    ["hello"]: async () => ({ default: () => <div>hello</div> }),
+    ["subdir/index"]: async () => ({ default: () => <div>subdir index</div> }),
+    ["subdir/hello"]: async () => ({ default: () => <div>subdir hello</div> }),
+  };
+  const eagle = new Eagle(routes2, hydrateRoutes);
+  const response1 = await eagle.handleRequest(
+    new Request("http://localhost:8787")
+  );
+  expect(await response1.text()).toMatchInlineSnapshot(`
+    "<html lang=\\"en\\"><head><meta charSet=\\"UTF-8\\"/><meta http-equiv=\\"X-UA-Compatible\\" content=\\"IE=edge\\"/><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1.0\\"/><title>Document</title></head><body><div id=\\"eagle-root\\"><div>hello world</div></div>
+    <script type=\\"module\\">
+    function hydrate(){}
+    </script></body></html>"
+  `);
+
+  const response2 = await eagle.handleRequest(
+    new Request("http://localhost:8787", {
+      method: "POST",
+    })
+  );
+  expect(await response2.text()).toMatchInlineSnapshot(
+    '"<div>without session</div>"'
+  );
 });
