3e4e56a2ec20443b84f93ad113f2d264c1618504
toyamarinyon <toyamarinyon@gmail.com>
2022-08-27 02:52:57 +0900

    update

--- apps/example/src/pages/index.tsx
+++ apps/example/src/pages/index.tsx
@@ -9,16 +9,21 @@ function AnotherComponent() {
   return <h1>Hello!</h1>;
 }
 
-export const pageProps: PageProps<inferEagleSession<typeof app>> = async ({
-  session,
-}) => {
+export const pageProps: PageProps<
+  inferEagleSession<typeof app>,
+  {
+    message: string;
+  }
+> = async ({ session }) => {
   const username = session.userId ?? "guest";
   return {
     message: username,
   };
 };
 
-export default function HelloWorld(props: ReturnType<typeof pageProps>) {
+export default function HelloWorld(
+  props: Awaited<ReturnType<typeof pageProps>>
+) {
   const [count, setCount] = useState(0);
   return (
     <div className={container}>
